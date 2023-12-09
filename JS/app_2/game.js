2651769969105abc56228909fab0c1777cb260d9
Arthur Richard <arthurrichard66@gmail.com>
2023-12-09 19:38:02 -0300

    finaliza moscas aparecendo aleatoriamente

--- JS/app_2/game.js
+++ JS/app_2/game.js
@@ -12,8 +12,11 @@ ajustaTamanhoTela()
 
 function posicaoRandomica() {
 
-var posicaoX = Math.floor(Math.random() * largura)
-var posicaoY = Math.floor(Math.random() * altura)
+var posicaoX = Math.floor(Math.random() * largura) - 90
+var posicaoY = Math.floor(Math.random() * altura) - 90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
 
 console.log(posicaoX, posicaoY)
 
@@ -21,8 +24,28 @@ console.log(posicaoX, posicaoY)
 //criar elemento html
 var mosquito = document.createElement('img')
 mosquito.src = 'imagens/mosca.png'
-mosquito.className = 'mosquito1'
+mosquito.className = tamanhoAleatorio()
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
 
 document.body.appendChild(mosquito)
 
+console.log()
+
+}
+
+function tamanhoAleatorio() {
+        var classe = Math.floor(Math.random() * 3)
+
+        switch(classe) {
+                case 0:
+                        return 'mosquito1'
+                        
+                case 1:
+                        return 'mosquito2'
+
+                case 2:
+                        return 'mosquito3'
+        }
 }
