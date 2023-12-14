be8299b346eca851b9415ec0a071960d87ea2eb5
Arthur Richard <arthurrichard66@gmail.com>
2023-12-13 21:18:32 -0300

    cria cenario

--- JS/app_2/game.js
+++ JS/app_2/game.js
@@ -12,6 +12,12 @@ ajustaTamanhoTela()
 
 function posicaoRandomica() {
 
+//remover o mosquito anterior (sendo existente)
+if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+}
+
+
 var posicaoX = Math.floor(Math.random() * largura) - 90
 var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -28,6 +34,7 @@ mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
 mosquito.style.left = posicaoX + 'px'
 mosquito.style.top = posicaoY + 'px'
 mosquito.style.position = 'absolute'
+mosquito.id = 'mosquito'
 
 document.body.appendChild(mosquito)
 
