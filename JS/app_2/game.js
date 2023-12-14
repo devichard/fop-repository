629ad2943e4fe4e1b8b7d972a5ba80c4739ef9b3
Arthur Richard <arthurrichard66@gmail.com>
2023-12-14 14:54:24 -0300

    finaliza game over

--- JS/app_2/game.js
+++ JS/app_2/game.js
@@ -19,7 +19,8 @@ if(document.getElementById('mosquito')) {
 
         //console.log('elemento selecionado foi: v' + vidas)
         if(vidas > 3) {
-                alert('Interromper o jogo (game over)')
+                
+                window.location.href = fim_de_jogo.html
         } else {
                 document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
 
