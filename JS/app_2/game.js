f050a611beeba93e39570477a7f64065d34eec2b
Arthur Richard <arthurrichard66@gmail.com>
2023-12-14 15:19:19 -0300

    adiciona cronometro

--- JS/app_2/game.js
+++ JS/app_2/game.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 15
 
 function ajustaTamanhoTela() {
         altura = window.innerHeight
@@ -10,6 +11,21 @@ function ajustaTamanhoTela() {
 }
 
 ajustaTamanhoTela()
+
+var cronometro = setInterval(function() {
+        
+        tempo -= 1
+
+        if(tempo < 0) {
+                clearInterval(cronometro)
+                clearInterval(criaMosca)
+             alert('Vitória!')   
+        } else {
+                document.getElementById('cronometro').innerHTML = tempo
+        }
+        
+
+}, 1000)
 
 function posicaoRandomica() {
 
