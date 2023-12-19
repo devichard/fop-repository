4a42fffd9d453c34a9153ad7db608ac240a8ccff
Arthur Richard <arthurrichard66@gmail.com>
2023-12-18 21:53:57 -0300

    finaliza configuração

--- JS/app_2/game.js
+++ JS/app_2/game.js
@@ -3,6 +3,8 @@ var largura = 0
 var vidas = 1
 var tempo = 15
 
+alert(window.location.href)
+
 function ajustaTamanhoTela() {
         altura = window.innerHeight
         largura = window.innerWidth
