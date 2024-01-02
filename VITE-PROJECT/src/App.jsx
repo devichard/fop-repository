7d8421f3b319c83f8fa33234f1b9140e96c9e04f
Arthur Richard <arthurrichard66@gmail.com>
2024-01-02 20:12:06 -0300

    vite project 2

---
+++ VITE-PROJECT/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return ( 
+  <div className="flex flex-col items-center justify-center h-screen bg-gray-950">
+    <div className="flex flex-col h-1/2 w-1/2 border border-red-900">
+        <h1 className="text-slate-200 font-bold text-4lg mb-2.5">FANOTA - Cadastro de Produtos</h1>
+      <div className="h-full bg-slate-900"></div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
