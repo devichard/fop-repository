011b881e89b9d42247ebb80c9aa0f5e4f24e1b71
Arthur Richard <arthurrichard66@gmail.com>
2024-01-02 15:29:20 -0300

    useEffect & useState

--- VITE/src/App.jsx
+++ VITE/src/App.jsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
 
+  const [advice, setAdvice] = useState("");
+  const [counter, setCounter] = useState(0);
+  
+
+  const pegarAprendizado = async () => {
+    setAdvice("Aguarde...");
+    setCounter((valorAtual) => valorAtual + 1);
+
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+
+    setAdvice(data.slip.advice);
+};
+
+    useEffect(() => {
+      pegarAprendizado();
+    }, []); //sem nada nos [] executa somente 1x
+    
+
   return (
-    <>
-     <h1>chardexy</h1>
-    </>
-  )
+    <div className="p-10 text-center">
+      <h1 className="text-3xl font-bold text-center">FastFiscal - Brasil</h1>
+      <p>Aprendizado do Dia: </p>
+      <p>{advice}</p>
+      <button
+      onClick={pegarAprendizado} 
+      className="p-2.5 border border-blue-500 text-blue-500 rounded-lg ,mt-5"
+      >
+        Outro Aprendizado
+        </button>
+        <p className="mt-5">Foram dados {counter} aprendizados.</p>
+     </div>
+  );
 }
 
 export default App
---
+++ VITE/src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import "../../index.css";
+
+function Home() {
+    return <p>Home</p>;
+}
+
+export default Home;
